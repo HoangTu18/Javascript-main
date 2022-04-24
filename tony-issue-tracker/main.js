d84b26086959fed55569bd0e5809d7e33b432b31
Tony Nguyen <tony.nguyen@usonialabs.com>
2022-04-24 11:18:08 +0700

    move component

--- tony-issue-tracker/main.js
+++ tony-issue-tracker/main.js
@@ -1,6 +1,6 @@
 // let todosLists = [];
 
-function renderTodos(todos) {
+function renderTodos(todos = []) {
   const issuesList = document.getElementById('issuesList');
 
   // reset 
@@ -16,7 +16,7 @@ function renderTodos(todos) {
           <h5 class="card-title">${todos[index].description}</h5>
           <p class="card-text"><span class="badge badge-primary">${todos[index].severity}</span></p>
           <div class="d-flex justify-content-end">
-            <button type="submit" class="btn btn-primary" style="margin-right: 10px;" onclick="changeStatusTodo(\'${todos[index].id}\')">Close</button>
+            <button type="submit" class="btn btn-primary" style="margin-right: 10px;" onclick="changeStatusTodo(\'${todos[index].id}\')">Edit</button>
             <button type="submit" class="btn btn-danger" onclick="deleteTodo(\'${todos[index].id}\')">Delete</button>
           </div>
         </div>
@@ -27,12 +27,19 @@ function renderTodos(todos) {
 };
 
 // fetch list todos
-fetch('https://tony-json-server.herokuapp.com/api/todos')
+fetch(`https://tony-json-server.herokuapp.com/api/todos?page=1&limit=10`)
     .then(res => res.json())
     .then(data => {
+      console.log(data)
       const todos = data.data;
       renderTodos(todos);
     })
+
+fetch(`https://tony-json-server.herokuapp.com/api/todos/412avae-4712-4d21-n452-d31e8648771e`)
+  .then(res => res.json())
+  .then(data => {
+    console.log('fetch single todo: ', data)
+  })
 
 
     
